954eea3c0998f3087689faf8fafe74333c6e9678
dankrasilnikov <krasilnikov.orchid@gmail.com>
2025-01-11 18:57:18 +0100

    fix: optimized selection sort

--- src/Algorithms/sorting/SelectionSort.ts
+++ src/Algorithms/sorting/SelectionSort.ts
@@ -1,7 +1,8 @@
 export const selectionSort = (arr: number[]) => {
     for(let j = 0; j< arr.length - 1; j++) {
 
-        let i = j;
+        let key = arr[j];
+        let i = j + 1;
         let min = j;
 
         while (i < arr.length) {
@@ -9,13 +10,32 @@ export const selectionSort = (arr: number[]) => {
             i++;
         }
 
-        let tempValue = arr[j];
-
         arr[j] = arr[min];
-        arr[min] = tempValue;
+        arr[min] = key;
     }
 
     return arr;
 }
 
-console.log(selectionSort([0, 55, 2, 3, 77, 31, 3252, 64, 1161, 3, 2]))
+console.log(selectionSort([0, 55, 2, 3, 77, 31, 3252, 64, 1161, 3, 2]));
+
+export const selectionSort2 = (arr: number[]): number[] => {
+    const n = arr.length;
+    for (let j = 0; j < n - 1; j++) {
+        let minIndex = j;
+
+        for (let i = j + 1; i < n; i++) {
+            if (arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        if (minIndex !== j) {
+            [arr[j], arr[minIndex]] = [arr[minIndex], arr[j]];
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort2([0, 55, 2, 3, 77, 31, 3252, 64, 1161, 3, 2]));
+
